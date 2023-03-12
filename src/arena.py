02a5d230a1129d13490174b42f3e4c9b2ff62252
David Ferreira <davidferreira@Air-de-David-2.home>
2023-03-12 19:18:35 +0000

    more progress on Arena

--- src/arena.py
+++ src/arena.py
@@ -1,5 +1,5 @@
-import pygame
-
+'''import pygame
+from player import Player
 ARENA_WIDTH = 30
 ARENA_HEIGHT = 50
 BLOCK_SIZE = 30  
@@ -9,12 +9,13 @@ pygame.init()
 
 
 screen = pygame.display.set_mode((ARENA_WIDTH * BLOCK_SIZE, ARENA_HEIGHT * BLOCK_SIZE))
+player1=Player("David")
 
 # Define colors
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 WHITE = (255, 255, 255)
-player_name = "Player 1"
+player_name = player1.get_name()
 score = 0
 font = pygame.font.SysFont(None, 30)
 
@@ -43,3 +44,81 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+            '''
+import pygame
+from player import Player
+
+ARENA_WIDTH = 20
+ARENA_HEIGHT = 15
+BLOCK_WIDTH =50
+BLOCK_HEIGHT=50
+BLOCK_SIZE=40 
+
+# Define colors
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Initialize Pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((ARENA_WIDTH * BLOCK_SIZE, ARENA_HEIGHT * BLOCK_SIZE))
+
+# Create the player
+player = Player("David", 0, 0)
+
+# Set the clock
+clock = pygame.time.Clock()
+
+# Set the font
+font = pygame.font.SysFont(None, 30)
+
+# Define the game loop
+while True:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Handle key presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.move_left()
+            elif event.key == pygame.K_RIGHT:
+                player.move_right()
+            elif event.key == pygame.K_UP:
+                player.move_up()
+            elif event.key == pygame.K_DOWN:
+                player.move_down()
+
+    # Draw the background
+    screen.fill(GRAY)
+
+    # Draw the grid
+    for i in range(ARENA_WIDTH):
+        for j in range(ARENA_HEIGHT):
+            rect = pygame.Rect(i * BLOCK_SIZE, j * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+
+    # Draw the player
+    player_rect = pygame.Rect(player.x * BLOCK_SIZE, player.y * BLOCK_SIZE, BLOCK_WIDTH, BLOCK_HEIGHT)
+    pygame.draw.rect(screen, RED, player_rect)
+
+    # Draw the score
+    score_text = font.render(f"Score: {player.score}", True, WHITE)
+    screen.blit(score_text, (10, 10))
+    level_text = font.render(f"Level: {player.level}", True, WHITE)
+    screen.blit(level_text, (10, 30))
+    
+
+
+    # Update the display
+    pygame.display.update()
+
+    # Set the frame rate
+    clock.tick(10)
+           
--- src/player.py
+++ src/player.py
@@ -2,9 +2,12 @@ import pygame
 
 class Player:
 
-    def player(self,name):
+    def __init__(self,name,x,y):
         self.name=name
         self.score=0
+        self.x=0
+        self.y=0
+        self.level=1
 
 
     def get_name(self):
@@ -13,8 +16,21 @@ class Player:
     def get_score(self): 
         return self.score
 
+    def get_level(self):
+        return self.level    
+
     def update_score(self,points):
         self.score+=points 
+
+    def move_up(self):
+        self.y -=1   
+    def move_left(self):
+        self.x -= 1  
+    def move_right(self):
+        self.x +=1   
+
+    def move_down(self):
+        self.y += 1   
 
 
     def _str(self):
