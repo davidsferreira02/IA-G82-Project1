86a5b990cdfa0c4545a0df0d532c37b551cbfa12
David Ferreira <davidferreira@MacBook-Air-de-David-2.local>
2023-03-16 14:06:27 +0000

    limites no bloco

--- src/menu.py
+++ src/menu.py
@@ -13,7 +13,9 @@ SCREEN_HEIGHT = 480
 
 # Set the font
 font = pygame.font.SysFont(None, 30)
-start_button = pygame.Rect(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 25, 100, 50)
+start_button = pygame.Rect(SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2 - 25, 150, 50)
+instructions_button = pygame.Rect(SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2 + 50 , 150, 50)
+quit_button=pygame.Rect(SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2 + 120, 150, 50)
 
 # Create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -37,15 +39,26 @@ while True:
     screen.fill(WHITE)
 
     # Draw the title
-    title_text = font.render("My Game", True, BLACK)
-    screen.blit(title_text, (SCREEN_WIDTH // 2 - title_text.get_width() // 2, 100))
+    title_text = font.render(" Space Block – Roll the Block ", True, BLACK)
+    screen.blit(title_text, (((SCREEN_WIDTH // 2 )+80) - title_text.get_width() // 2, 100))
 
     # Draw the instructions
-    instructions_text = font.render("Start", True, BLACK)
-    screen.blit(instructions_text, (start_button.centerx - instructions_text.get_width() // 2, start_button.centery - instructions_text.get_height() // 2))
+    start_text = font.render("Start", True, BLACK)
+    screen.blit(start_text, (start_button.centerx - start_text.get_width() // 2, start_button.centery - start_text.get_height() // 2))
+
+    instructions_text = font.render("Instructions", True, BLACK)
+    screen.blit(instructions_text, (instructions_button.centerx - instructions_text.get_width() // 2, instructions_button.centery - instructions_text.get_height() // 2))
+
+    quit_text = font.render("Quit", True, BLACK)
+    screen.blit(quit_text, (quit_button.centerx - quit_text.get_width() // 2, quit_button.centery - quit_text.get_height() // 2))
+
+
+   
 
     # Draw the start button
     pygame.draw.rect(screen, BLACK, start_button, 2)
+    pygame.draw.rect(screen,BLACK,instructions_button,2)
+    pygame.draw.rect(screen,BLACK,quit_button,2)
 
     # Update the display
     pygame.display.update()
--- src/player.py
+++ src/player.py
@@ -1,27 +1,41 @@
 import pygame
 
+ARENA_WIDTH = 20
+ARENA_HEIGHT = 15
+BLOCK_WIDTH = 50
+BLOCK_HEIGHT = 70
+BLOCK_SIZE = 40
+
+
 class Player:
+
+    
+    
+    
 
     def __init__(self,name,x,y):
         self.score=0
         self.x=0
         self.y=0
-        self.level=1
-    
+        self.level=1    
  
 
     def update_score(self):
         self.score+=1 
 
     def move_up(self):
-        self.y -=1   
+        if(self.y>0):
+            self.y -=1   
     def move_left(self):
-        self.x -= 1  
+        if(self.x>0):
+           self.x -= 1  
     def move_right(self):
-        self.x +=1   
+        if(self.x <ARENA_WIDTH -1):
+            self.x +=1   
 
     def move_down(self):
-        self.y += 1
+        if (self.y <ARENA_HEIGHT -1):
+            self.y += 1
 
     def update_level(self):
         self.level+=1
